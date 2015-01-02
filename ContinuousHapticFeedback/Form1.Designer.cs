0f2856e771f03c616d9a37c20800328dec59e119
Jingun Jung <zzongaly@gmail.com>
2015-01-02 15:22:50 +0900

    원형 이동(X 값)에 따라  모드 변경 및 함수 실행 부분 생

--- ContinuousHapticFeedback/Form1.Designer.cs
+++ ContinuousHapticFeedback/Form1.Designer.cs
@@ -38,6 +38,9 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
--- ContinuousHapticFeedback/Form1.cs
+++ ContinuousHapticFeedback/Form1.cs
@@ -12,14 +12,73 @@ namespace ContinuousHapticFeedback
 {
     public partial class Form1 : Form
     {
+        int mode = 0;
+        const int idleMode = 0, zoomInMode = 1, zoomOutMode = 2;
+
+        int initMouseX;
+        int prevMouseX;
+        int threshold = 10;
+
+
         public Form1()
         {
+            initMouseX = -1;
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
+                
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            switch (mode)
+            {
+                case idleMode:
+                    if (e.X - initMouseX > threshold)
+                    {
+                        mode = zoomInMode;
+                        prevMouseX = e.X;
+                    }
+                    else if(e.X - initMouseX < -threshold)
+                    {
+                        mode = zoomOutMode;
+                        prevMouseX = e.X;
+                    }
+                    break;
+                case zoomInMode:
+                    if (e.X > prevMouseX)
+                        zoomIn(e.X - prevMouseX);
+                    break;
+                case zoomOutMode:
+                    if (e.X < prevMouseX)
+                        zoomOut(prevMouseX - e.X);
+                    break;
+                default:
+                    break;
+            }   
+        }
 
+        private void zoomIn(int factor)
+        {
+
+        }
+
+        private void zoomOut(int factor)
+        {
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            initMouseX = -1;
+            mode = idleMode;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            initMouseX = e.X;            
         }
     }
 }

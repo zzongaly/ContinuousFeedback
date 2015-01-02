2840cd18b418f7da27a341fd8e0d3eaedaafb34c
Jingun Jung <zzongaly@gmail.com>
2015-01-02 17:07:02 +0900

    1픽셀 씩 줌인, 줌아웃 그림 위에서 드래그 시 가능 이미지 왼쪽 위으로 확대됨

--- ContinuousHapticFeedback/Form1.Designer.cs
+++ ContinuousHapticFeedback/Form1.Designer.cs
@@ -28,24 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(448, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1024, 768);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(846, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ContinuousHapticFeedback/Form1.cs
+++ ContinuousHapticFeedback/Form1.cs
@@ -12,27 +12,62 @@ namespace ContinuousHapticFeedback
 {
     public partial class Form1 : Form
     {
-        int mode = 0;
+        int mode;
         const int idleMode = 0, zoomInMode = 1, zoomOutMode = 2;
 
         int initMouseX;
         int prevMouseX;
         int threshold = 10;
+        double coef = 0.001;
+        Size size;
 
+        Bitmap currentBitmap, stoneBitmap;
 
         public Form1()
         {
+            mode = idleMode;
             initMouseX = -1;
+            prevMouseX = -1;
+            
+            stoneBitmap = new Bitmap(ContinuousHapticFeedback.Properties.Resources.stone);
+            size = stoneBitmap.Size;
+
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            this.pictureBox1.Image = stoneBitmap;
         }
-                
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
+
+        private void zoomIn(int factor)
         {
+            zoom(factor);
+        }
+        private void zoomOut(int factor)
+        {
+            zoom(-factor);
+        }
+
+        private void zoom(int factor)
+        {
+            int zoomFactor = factor > 0 ? 1 : -1;
+            size = new Size((int)(size.Width + zoomFactor), (int)(size.Height + zoomFactor));
+            currentBitmap = new Bitmap(stoneBitmap, size);
+            this.pictureBox1.Image = currentBitmap;
+        }        
+        
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!Control.MouseButtons.HasFlag(MouseButtons.Left))
+            {
+                this.pictureBox1_MouseUp(sender, e);
+                return;
+            }
+        
+            if (initMouseX == -1)
+                initMouseX = e.X;
+
             switch (mode)
             {
                 case idleMode:
@@ -40,45 +75,49 @@ namespace ContinuousHapticFeedback
                     {
                         mode = zoomInMode;
                         prevMouseX = e.X;
+                        textBox1.Text = "ZoomIn";
                     }
-                    else if(e.X - initMouseX < -threshold)
+                    else if (e.X - initMouseX < -threshold)
                     {
                         mode = zoomOutMode;
                         prevMouseX = e.X;
+                        textBox1.Text = "ZoomOut";
                     }
                     break;
                 case zoomInMode:
                     if (e.X > prevMouseX)
+                    {
                         zoomIn(e.X - prevMouseX);
+                    }
+                    prevMouseX = e.X;
                     break;
                 case zoomOutMode:
                     if (e.X < prevMouseX)
+                    {
                         zoomOut(prevMouseX - e.X);
+                    }
+                    prevMouseX = e.X;
                     break;
                 default:
                     break;
             }   
         }
 
-        private void zoomIn(int factor)
-        {
-
-        }
-
-        private void zoomOut(int factor)
-        {
-
-        }
-
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
         {
             initMouseX = -1;
             mode = idleMode;
+            textBox1.Text = "Idle";
         }
 
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            initMouseX = e.X;            
+            initMouseX = -1;
+            mode = idleMode;
+            textBox1.Text = "Idle";
         }
+
+       
+
     }
 }
